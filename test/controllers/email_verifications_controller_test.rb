fa956ff59f6aca4c5603159dcb47e85e06454403
増子祐貴 <masukoyuki@mashikoyuukinoMacBook-Air.local>
2026-04-11 12:28:55 +0900

    test修正

--- test/controllers/email_verifications_controller_test.rb
+++ test/controllers/email_verifications_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class EmailVerificationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get email_verifications_new_url
+    get new_email_verification_url
     assert_response :success
   end
 
   test "should get create" do
-    get email_verifications_create_url
+    get email_verification_url
     assert_response :success
   end
 end
